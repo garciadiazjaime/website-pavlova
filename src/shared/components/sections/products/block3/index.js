0df172f1a734b554ff176fddebbff0cc67954586
jaime <garciadiazjaime@gmail.com>
2016-05-10 19:41:34 -0500

    blocks clases

--- src/shared/components/sections/products/block3/index.js
+++ src/shared/components/sections/products/block3/index.js
@@ -1,37 +1,44 @@
 /* eslint max-len: [2, 500, 4] */
 import React from 'react';
-import _ from 'lodash';
 import { Link } from 'react-router';
+import _ from 'lodash';
 import Carousel from '../../../elements/carousel';
-import sanitizeUtil from '../../../../utils/sanitize';
+import getSlug from '../../../../utils/slug';
 const style = require('./style.scss');
 
-
 export default class Block3 extends React.Component {
+
+  getColumnSlider(slides, item) {
+    const carouselClasses = {
+      inner: style.inner,
+      controls: {
+        base: style.controls,
+        prev: style.prev,
+        next: style.next,
+      },
+    };
+    const slug = getSlug(item);
+    return (<Carousel id={'carousel-clases-' + slug} interval={8000} indicators={false} classes={carouselClasses}>
+      {this.renderItems(slides)}
+    </Carousel>);
+  }
+
+  getColumnContent(titles, paragraphs, buttons) {
+    return (<div>
+      <h2>{titles.title1}</h2>
+      <p>{paragraphs.paragraph1}</p>
+      <Link to={buttons.button1.href}>{buttons.button1.title}</Link>
+      { buttons.button2 ? <Link to={buttons.button2.href}>{buttons.button2.title}</Link> : null}
+
+    </div>);
+  }
 
   renderItems(data) {
     if (_.isArray(data) && data.length) {
       return data.map((item, index) => {
         const className = index === 0 ? 'active' : '';
-        const imgUrl = item.image.length ? item.image.replace('www.dropbox.com', 'dl.dropboxusercontent.com') : '/images/demo.png';
         return (<div className={'item ' + className + ' ' + (style.item || '')} key={index}>
-          <div className={'container-fluid ' + style.wrapper}>
-            <div className="row">
-              <div className="col-xs-12 col-sm-8 col-sm-offset-2 col-md-10 col-md-offset-1 ">
-                <img className={style.image} src={imgUrl} alt={item.title} />
-                <hr className={style.hr}/>
-              </div>
-              <div className="col-xs-12 col-sm-4 col-sm-offset-2 col-md-5 col-md-offset-1">
-                <h3 className={style.subtitle}>{item.title}</h3>
-                <p dangerouslySetInnerHTML={sanitizeUtil(item.content)} className={style.paragraph} />
-              </div>
-              <div className="col-xs-12 col-sm-4 col-md-5">
-                <Link to={item.button_url} title={item.button_title} className={style.button}>
-                  {item.button_title}
-                </Link>
-              </div>
-            </div>
-          </div>
+          <img src={item.image} alt={item.title} />
         </div>);
       });
     }
@@ -39,29 +46,29 @@ export default class Block3 extends React.Component {
   }
 
   render() {
-    const { sliderId } = this.props;
-    const { slides, titles } = this.props.data;
-    const carouselClasses = {
-      inner: style.inner,
-      controls: {
-        base: style.controls,
-        prev: style.prev,
-        next: style.next,
-        arrow: style.arrow,
-      },
-    };
-    return _.isArray(slides) && slides.length && titles ? (<div>
-      <div>
-        <h2 className={style.title}>{titles.title1}</h2>
+    const { data, type } = this.props;
+    const { titles, slides, paragraphs, buttons } = data;
+    const sliderEl = this.getColumnSlider(slides, titles.title1);
+    const contentEl = this.getColumnContent(titles, paragraphs, buttons);
+
+    return !_.isEmpty(this.props.data) ? (<div>
+      <div className={'container-fluid'}>
+        <div className="row">
+          <div className="col-sm-6 col-xs-12">
+            {type === 'SLIDER_CONTENT' ? sliderEl : contentEl}
+          </div>
+          <div className="col-sm-6 col-xs-12">
+            {type === 'SLIDER_CONTENT' ? contentEl : sliderEl}
+          </div>
+        </div>
       </div>
-      <Carousel id={sliderId} interval={8000} indicators={false} classes={carouselClasses}>
-        {this.renderItems(slides)}
-      </Carousel>
     </div>) : null;
   }
 }
 
 Block3.propTypes = {
-  data: React.PropTypes.object,
-  sliderId: React.PropTypes.string.isRequired,
+  data: React.PropTypes.object.isRequired,
+  classes: React.PropTypes.object,
+  type: React.PropTypes.string.isRequired,
+  variations: React.PropTypes.object.isRequired,
 };

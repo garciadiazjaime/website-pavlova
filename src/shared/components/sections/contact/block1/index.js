91f41e5b237483679c8fbe3b0658845a1c2220e3
jaime <garciadiazjaime@gmail.com>
2016-05-10 19:57:09 -0500

    blocks contact

--- src/shared/components/sections/contact/block1/index.js
+++ src/shared/components/sections/contact/block1/index.js
@@ -9,11 +9,10 @@ const style = require('./style.scss');
 export default class Block1 extends React.Component {
 
   render() {
-    const { images, buttons } = this.props.data;
+    const { images } = this.props.data;
     const imgUrl = images && images.image1 && images.image1.src ? images.image1.src.replace('www.dropbox.com', 'dl.dropboxusercontent.com') : '/images/contacto_map.png';
     return !_.isEmpty(this.props.data) ? (<div>
       <img cassName={style.image} src={imgUrl} alt={images.image1.alt} />
-      <a className={style.button} href={buttons.button1.href} target="_blank">{buttons.button1.title}</a>
     </div>) : null;
   }
 }

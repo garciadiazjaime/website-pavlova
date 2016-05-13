cdff06b40f8ebe770e4a66c758a133ad0b8f630a
jaime <garciadiazjaime@gmail.com>
2016-05-12 19:55:39 -0500

    revert carousel home

--- src/shared/components/sections/home/block1/index.js
+++ src/shared/components/sections/home/block1/index.js
@@ -47,7 +47,7 @@ export default class Block1 extends React.Component {
       },
     };
     return !_.isEmpty(this.props.data) ? (<div>
-      <Carousel id="carousel-home-block-1" interval={8000} classes={carouselClasses}>
+      <Carousel id="carousel-home-block-1" interval={8000} indicators={false} controls={false} classes={carouselClasses}>
         {this.renderItems(slides)}
       </Carousel>
     </div>) : null;
